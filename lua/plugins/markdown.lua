adb6559f5f03e7bbc6f43af0a01be55c5914c970
Eran Heres <autocron@eran.com>
2025-01-05 23:03:31 +0200

    add markdown support

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,3 +1,145 @@
-return  {
-  "rafamadriz/friendly-snippets", enabled = false
+return {
+  {
+    "dhruvasagar/vim-table-mode",
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      checkbox = {
+        enabled = true,
+        position = "overlay",
+        unchecked = {
+          icon = "🔲",
+          highlight = "RenderMarkdownUnchecked",
+          scope_highlight = nil,
+        },
+        checked = {
+          icon = "✅",
+          highlight = "RenderMarkdownChecked",
+          scope_highlight = nil,
+        },
+        custom = {
+          todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          assigned = { raw = "[>]", rendered = "👀", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          cancelled = { raw = "[~]", rendered = "❌", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          repeated = { raw = "[r]", rendered = "🔄", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+        },
+      },
+      code = {
+        enabled = true,
+        sign = true,
+        style = "full",
+        position = "left",
+        language_pad = 0,
+        language_name = true,
+        disable_background = { "diff" },
+        width = "full",
+        left_margin = 2,
+        left_pad = 2,
+        right_pad = 4,
+        min_width = 0,
+        border = "thin",
+        above = "▄",
+        below = "▀",
+        highlight = "RenderMarkdownCode",
+        highlight_inline = "RenderMarkdownCodeInline",
+        highlight_language = nil,
+      },
+      heading = {
+        -- Turn on / off heading icon & background rendering
+        enabled = true,
+        -- Additional modes to render headings
+        render_modes = false,
+        -- Turn on / off any sign column related rendering
+        sign = true,
+        -- Determines how icons fill the available space:
+        --  right:   '#'s are concealed and icon is appended to right side
+        --  inline:  '#'s are concealed and icon is inlined on left side
+        --  overlay: icon is left padded with spaces and inserted on left hiding any additional '#'
+        position = "overlay",
+        -- Replaces '#+' of 'atx_h._marker'
+        -- The number of '#' in the heading determines the 'level'
+        -- The 'level' is used to index into the list using a cycle
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        -- Added to the sign column if enabled
+        -- The 'level' is used to index into the list using a cycle
+        signs = { "󰫎 " },
+        -- Width of the heading background:
+        --  block: width of the heading text
+        --  full:  full width of the window
+        -- Can also be a list of the above values in which case the 'level' is used
+        -- to index into the list using a clamp
+        width = "full",
+        -- Amount of margin to add to the left of headings
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        -- Margin available space is computed after accounting for padding
+        -- Can also be a list of numbers in which case the 'level' is used to index into the list using a clamp
+        left_margin = 0,
+        -- Amount of padding to add to the left of headings
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        -- Can also be a list of numbers in which case the 'level' is used to index into the list using a clamp
+        left_pad = 0,
+        -- Amount of padding to add to the right of headings when width is 'block'
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        -- Can also be a list of numbers in which case the 'level' is used to index into the list using a clamp
+        right_pad = 0,
+        -- Minimum width to use for headings when width is 'block'
+        -- Can also be a list of integers in which case the 'level' is used to index into the list using a clamp
+        min_width = 0,
+        -- Determines if a border is added above and below headings
+        -- Can also be a list of booleans in which case the 'level' is used to index into the list using a clamp
+        border = false,
+        -- Always use virtual lines for heading borders instead of attempting to use empty lines
+        border_virtual = false,
+        -- Highlight the start of the border using the foreground highlight
+        border_prefix = false,
+        -- Used above heading for border
+        above = "▄",
+        -- Used below heading for border
+        below = "▀",
+        -- The 'level' is used to index into the list using a clamp
+        -- Highlight for the heading icon and extends through the entire line
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        -- The 'level' is used to index into the list using a clamp
+        -- Highlight for the heading and sign icons
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+      },
+      -- heading = {
+      --   sign = false,
+      --   icons = {},
+      -- },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
+  },
 }
